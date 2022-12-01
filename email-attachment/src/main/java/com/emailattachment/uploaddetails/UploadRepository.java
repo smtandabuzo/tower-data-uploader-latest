e3ecfc4cd84a02ee59db71e19d469ea237460ba3
Kibuluku <fabriceibols@gmail.com>
2022-12-01 04:53:14 +0200

    Delete email-attachment/src/main/java/com/emailattachment directory

--- email-attachment/src/main/java/com/emailattachment/uploaddetails/UploadRepository.java
+++
@@ -1,9 +0,0 @@
-package com.emailattachment.uploaddetails;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface UploadRepository extends JpaRepository<UploadDetails, Integer> {
-
-}
